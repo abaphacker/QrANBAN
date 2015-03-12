acfc8e09f34485de472185ae485fdf05d3e7289f
Harald Gutbrod <harald.gutbrod@gmail.com>
2015-03-12 22:27:14 +0100

    ***

--- img2kanban.py
+++ img2kanban.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
         for card in cards:
             i = i + 1
             filename = "/home/harry/python_workspace/cards/card%s.png" % i 
-            cv2.imwrite(filename,card)
+            card.highlight_card_in_img()
+            card.save_img(filename)
         
-        cv2.imshow('card', card)
-        cv2.waitKey()
+        cv2.imwrite("/home/harry/python_workspace/cards/all_cards.png", img)
+        cv2.imshow("QrANBAN", img)
+        cv2.waitKey(0)
+        
